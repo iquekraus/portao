6b848e33b1052c073f88e2d6660527376e859f20
iquekraus <carlos.kraus28@gmail.com>
2018-11-15 21:50:23 -0200

    Acrescentada view 'Users'

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -15,3 +15,16 @@ class EventComments(TimeStampedModel):
         verbose_name = 'Event Coment'
         verbose_name_plural = 'Event Coments'
         #ordering = ['-created']
+
+class EventUsers(TimeStampedModel):
+    username = models.CharField(max_length=25, verbose_name='Username')
+    password = models.CharField(max_length=15, verbose_name='Password')
+    email = models.CharField(max_length=250, verbose_name='Email')
+
+    def __str__(self):
+        return str(self.username)
+
+    class Meta:
+        verbose_name = 'Event User'
+        verbose_name_plural = 'Event Users'
+        #ordering = ['-created']
